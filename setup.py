a06af29c476d15a00dae3611bcc31f69c797a815
peat <peat@Kittiphats-MacBook-Pro.local>
2015-12-04 16:56:11 +0700

    Distribute through pipit

--- setup.py
+++ setup.py
@@ -5,18 +5,18 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='pymongo-document-modeling',
-    version='0.0.1',
+    version='0.9.0.dev3',
     description='PyMongo data modeling library',
     long_description=long_description,
     url='https://github.com/peatiscoding/pymongo-document-modeling',
-    author='Peatiscoding',
+    author='peatiscoding',
     author_email='freeuxer@gmail.com',
-    license='Apache',
+    license='Apache License, Version 2.0',
     # see: https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -26,7 +26,7 @@ setup(
         'Topic :: Database',
         'Topic :: Software Development :: Libraries',
     ],
-    keywords='pymongo database mongodb modeling',
+    keywords=['pymongo', 'database', 'mongodb', 'modeling'],
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     install_requires=['six', 'pymongo'],
 )
